ef14a5ef873fc9861a8a2e3a20a01c024448b93a
Berk Çolak <colak@intranet.com.tr>
2021-12-24 19:16:23 +0300

    Add project files.

---
+++ RustMacro.UI/Main/Main.cs
@@ -0,0 +1,23 @@
+﻿using RustMacro.UI.src.MouseEvent;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustMacro.UI.Main
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            MouseEventWin32.GetCursorPos(out Point lpPoint);
+            MouseEventWin32.SetCursorPos(lpPoint.X+10, lpPoint.Y-10);
+        }
+    }
+}
---
+++ RustMacro.UI/src/MouseEvent/MouseEventWin32.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RustMacro.UI.src.MouseEvent
+{
+    public class MouseEventWin32
+    {
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+        //Sol Tıkladı
+        //mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+        ////Sol Bıraktı
+        //mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+    }
+}
